0bc7cbebd7c31e38248c4db36fa55bad40111b41
Cory Lewis <cjl9703@yahoo.com>
2015-11-19 00:57:56 -0800

    A few new spritesheets
    the fire looks better now.
    i added in a new music button to match the sound effects button

--- src/Preload.js
+++ src/Preload.js
@@ -51,7 +51,7 @@ BasicGame.Preloader.prototype = {
         this.load.image("resetgame", "asset/menu/resetgame.png");
         this.load.image("thankyou", "asset/menu/thankyou.png");
         this.load.image("playBtn", "asset/menu/playbutton.png");
-        this.load.spritesheet('musicToggle', 'asset/music_status.png',135,160,2);
+        this.load.spritesheet('musicToggle', 'asset/music_status_new.png',210,160,2);
         this.load.spritesheet('soundfxToggle','asset/fx_status.png',210,160,2);
         this.load.spritesheet('woodBlocker', 'asset/wood_spritesheet.png',110,77,5);
         
